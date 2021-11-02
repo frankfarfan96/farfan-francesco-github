0c751b009374ba57095d2e76377071c7457dba82
frank <frankfarfan96@gmail.com>
2021-11-02 12:40:43 +0100

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Results}  from "./components/results/results";
 function App() {
   return (
     <div className="App">    
-      <Head />
+      <Head /> 
       <Results />
     </div>  
   );
